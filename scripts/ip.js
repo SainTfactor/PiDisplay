08e718b5bbd4deaaf3ae9696192835cbc13232c1
Sean Tyler Atwell <sta0003@auburn.edu>
2015-04-10 17:33:35 -0500

    IP Address and News now auto refresh

--- scripts/ip.js
+++ scripts/ip.js
@@ -1,6 +1,20 @@
-$(function(){		
-    //IP Address
-    $.ajax( "http://jsonip.com/" ).success(function(data){
-        $("#ipinfo>div").html("<div>SainTfactor Studios</div><div style='position: absolute; bottom: 0; left: 50%; transform: translateX(-50%); width: 100%;'>" + data.ip + "</div>");
-    });
+$(function(){	
+    var oneInterval;
+    
+    function GetIP()
+    {
+        //IP Address
+        $.ajax( "http://jsonip.com/" ).success(function(data){
+            $("#ipinfo>div").html("<div>SainTfactor Studios</div><div style='position: absolute; bottom: 0; left: 50%; transform: translateX(-50%); width: 100%;'>" + data.ip + "</div>");
+        });
+    }
+    
+    function advanceToNext()
+    {
+        GetIP();
+        clearInterval(oneInterval);
+        oneInterval = setInterval(function(){ advanceToNext(); }, 3600000);
+    }
+    
+    advanceToNext();
 });
--- scripts/news.js
+++ scripts/news.js
@@ -1,9 +1,21 @@
-$(function(){
-
-    //Ticker
-    $.ajax( "http://api.reddit.com/r/worldnews/hot?limit=100" ).success(function(data){
-        var newsTicker = "";
-        $.each(data.data.children, function(i, val){ newsTicker += val.data.title.substring(0, 100) + ((val.data.title.length > 100) ? "..." : "") + " &bull; "; });
-        $("#ticker>div").html("<marquee scrollamount='9'>" + newsTicker + "</marquee>");
-    });
+$(function(){    
+    var oneInterval;
+    
+    function GetNews()
+    {
+        $.ajax( "http://api.reddit.com/r/worldnews/hot?limit=100" ).success(function(data){
+            var newsTicker = "";
+            $.each(data.data.children, function(i, val){ newsTicker += val.data.title.substring(0, 100) + ((val.data.title.length > 100) ? "..." : "") + " &bull; "; });
+            $("#ticker>div").html("<marquee scrollamount='9'>" + newsTicker + "</marquee>");
+        });
+    }
+    
+    function advanceToNext()
+    {
+        GetNews();
+        clearInterval(oneInterval);
+        oneInterval = setInterval(function(){ advanceToNext(); }, 3600000);
+    }
+    
+    advanceToNext();
 });
